37687ccd6ab0b9bf1de14551448b725f8289c2cb
Chase <crebsto1@students.towson.edu>
2025-11-08 17:32:10 -0500

    parser implementation for task 2

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,29 @@
-use crate::tokens::Token;
+use crate::lexer::Token;
+
+pub trait SyntaxAnalyzer {
+    fn parse_lolcode(&mut self) -> Result<(), String>;
+    fn parse_head(&mut self) -> Result<(), String>;
+    fn parse_title(&mut self) -> Result<(), String>;
+    fn parse_comment(&mut self) -> Result<(), String>;
+    fn parse_body(&mut self) -> Result<(), String>;
+    fn parse_paragraph(&mut self) -> Result<(), String>;
+    fn parse_inner_paragraph(&mut self) -> Result<(), String>;
+    fn parse_inner_text(&mut self) -> Result<(), String>;
+    fn parse_variable_define(&mut self) -> Result<(), String>;
+    fn parse_variable_use(&mut self) -> Result<(), String>;
+    fn parse_bold(&mut self) -> Result<(), String>;
+    fn parse_italics(&mut self) -> Result<(), String>;
+    fn parse_list(&mut self) -> Result<(), String>;
+    fn parse_list_items(&mut self) -> Result<(), String>;
+    fn parse_inner_list(&mut self) -> Result<(), String>;
+    fn parse_audio(&mut self) -> Result<(), String>;
+    fn parse_video(&mut self) -> Result<(), String>;
+    fn parse_newline(&mut self) -> Result<(), String>;
+}
 
 pub struct Parser {
-    tokens: Vec<token>,
-    pos:usize;
+    tokens: Vec<Token>,
+    pos:usize,
 }
 
 impl Parser {
@@ -20,12 +41,13 @@ impl Parser {
     fn expect(&mut self, expected: &Token) -> Result <(), String> {
         if let Some(token) = self.peek() {
             if token == expected {
+                self.advance();
                 Ok(())
             }else {
-                Err(format!("Expected {:?}, found at {:?} at position {}", expected, token, self.pos))
+                Err(format!("Syntax Error: Expected {:?}, found {:?}", expected, token))
             }
         } else {
-            Err(format!("Unexpected end of input. Expected {:?}", expected))
+            Err(format!("Syntax Error: Unexpected end of input. Expected {:?}", expected))
         }
     }
 
@@ -35,34 +57,292 @@ impl Parser {
         self.parse_head()?;
         self.parse_body()?;
         self.expect(&Token::Kthxbye)?;
+
+        if self.peek().is_some() {
+            return Err(format!("Syntax Error: Content found after #KTHXBYE: {:?}", self.peek()));
+        }
+
         Ok(())
     }
     fn parse_comments(&mut self) -> Result<(), String> {
-        while let Some(Token:Obtw) = self.peek() {
+        while self.peek() == Some(&Token::Obtw)  {
+            self.advance();
+            let mut found_tldr = false;
+            while let Some(token) = self.peek() {
+                if token == &Token:: Tldr {
+                    self.advance();
+                    found_tldr = true;
+                    break;
+                }
+                self.advance();
+            }
+            if !found_tldr {
+                return Err("Syntax Error: Comment started with #OBTW but never closed with #TLDR.".to_string());
+            }
+    }
+    Ok(())
+}
+fn parse_head(&mut self) -> Result<(), String> {
+    if self.peek() == Some(&Token::Maek){
+        self.advance();
+        self.expect(&Token::Head)?;
+        self.parse_comments()?;
+        self.parse_title()?;
+        self.parse_comments()?;
+        self.expect(&Token::Oic)?;
+    }
+    Ok(())
+}
+
+
+fn parse_title(&mut self) -> Result<(), String> {
+    self.expect(&Token::Gimmeh)?;
+    self.expect(&Token::Title)?;
+
+    match self.peek(){
+        Some(Token::Text(_)) => self.advance(),
+        _ => return Err ("Syntax Error: #GIMMEH TITLE must be followed by text.".to_string()),
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
+
+
+fn parse_body(&mut self) -> Result<(), String> {
+    while self.peek() != Some(&Token::Kthxbye){
+
+        self.parse_comments()?;
+
+        match self.peek() {
+            Some(Token::Maek) => {
+                self.advance();
+
+                match self.peek() {
+                    Some(Token::Paragraf) => self.parse_paragraph()?,
+                    Some(Token::List) => self.parse_list()?,
+                    _ => return Err(format!("Syntax Error: Expected PARAGRAF or LIST after #MAEK, found, found {:?}", self.peek())),
+                }
+            },
+            Some(Token::IHaz) => self.parse_variable_define()?,
+            Some(Token::LemmeSee) => self.parse_variable_use()?,
+            Some(Token::Gimmeh) => self.parse_gimmeh_body_element()?,
+            Some(Token::Text(_)) => self.parse_inner_text()?,
+
+            Some(Token::I) => {
+                self.advance();
+                self.expect(&Token::IHaz)?;
+                self.parse_variable_define_core()?
+            },
+            Some(Token::Bold) | Some(Token::Italics) | Some(Token::Soundz(_)) | Some(Token::Vidz(_)) | Some(Token::Newline) => self.advance(),
+            _ => {
+                return Err(format!("Syntax Error: Unexpected token in document body: {:?}", self.peek()));
+            }
+        }
+    }
+    Ok(())
+}
+
+
+fn parse_paragraph(&mut self) -> Result<(), String> {
+    self.expect(&Token::Paragraf)?;
+
+    if self.peek() == Some(&Token:: I) {
+        self.advance();
+        self.expect(&Token::IHaz)?;
+        self.parse_variable_define_core()?;
+    }
+    while self.peek() != Some(&Token::Oic){
+        self.parse_inner_paragraph()?;
+    }
+    self.expect(&Token::Oic)?;
+    Ok(())
+}
+
+
+
+fn parse_inner_paragraph(&mut self) -> Result<(), String> {
+    self.parse_comments()?;
+
+    match self.peek() {
+        Some(Token::Gimmeh) => {
             self.advance();
             match self.peek() {
-                Some(Token:: Text(_)) => self.advance(),
-                _ => return Err ("Expected text after OBTW".to_string()),
+               Some(Token::Bold) => self.parse_bold()?,
+                Some(Token::Italics) => self.parse_italics()?,
+                Some(Token::Soundz(_)) => self.parse_audio()?, 
+                Some(Token::Vidz(_)) => self.parse_video()?,   
+                Some(Token::Newline) => self.parse_newline()?,
+                _ => return Err(format!("Syntax Error: Unknown #GIMMEH element inside PARAGRAF: {:?}", self.peek())),
             }
-            self.expect(&Token::Tldr)?;
+        },
+        Some(Token::Maek)=> {
+            self.advance();
+            match self.peek() {
+                Some(Token::List) => self.parse_list()?,
+                _ => return Err(format!("Syntax Error: Expected LIST after #MAEK inside PARAGRAF, found {:?}",self.peek()))
+            }
+        },
+        Some(Token::I) => {
+            return Err("Syntax Error: Variable definition #I HAZ... not allowed mid-paragraph.".to_string());
         }
-        OK(())
+        Some(Token::LemmeSee) => self.parse_variable_use()?,
+        Some(Token::Text(_)) => self.parse_inner_text()?,
+        _ => return Err (format!("Syntax Error: Unexpected token inside paragraph: {:?}", self.peek())),
     }
-// fn parse_head()
-//to be implemented
+    Ok(())
+}
+fn parse_variable_define(&mut self) -> Result<(), String> {
+    self.expect(&Token::IHaz)?;
+    self.parse_variable_define_core()
+}
 
-//fn parse_title()
-// to be implemented
+fn parse_variable_define_core(&mut self) -> Result<(), String> {
+    match self.peek() {
+        Some(Token::Text(_)) => self.advance(),
+        _ => return Err("Syntax Error: Expected variable name (Text) after #I HAZ.".to_string())
+    }
+    self.expect(&Token::ItIz)?;
 
-//fn parse_body
-//to be implemented
+    match self.peek(){
+        Some(Token::Text(_)) => self.advance(),
+        _ => return Err("Syntax Error: Expected variable value (Text) after #IT IZ.".to_string()),
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
+fn parse_variable_use(&mut self) -> Result<(), String> {
+    self.expect(&Token::LemmeSee)?;
 
-//fn parse_paragraph
-//to be implemented
+    match self.peek(){
+        Some(Token::Text(_)) => self.advance(),
+        _ => return Err ("Syntax Error: Expected variable name (Text) after #LEMME SEE.".to_string()),
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
 
-//fn parse_inner_paragraph
-//to be implemented
+fn parse_gimmeh_body_element(&mut self) -> Result<(), String> {
+    self.expect(&Token::Gimmeh)?;
+    match self.peek() {
+        Some(Token::Bold) => self.parse_bold(),
+        Some(Token::Italics) => self.parse_italics(),
+        Some(Token::Soundz(_)) => self.parse_audio(),
+        Some(Token::Vidz(_)) => self.parse_video(),
+        Some(Token::Newline) => self.parse_newline(),
+        _ => Err (format!("Syntax Error: Unexpected GIMMEH command in body: {:?}", self.peek())),
+    }
+ }
 
-// still need to handle variables, inline formatting and list structure (task 3)
 
+fn parse_audio(&mut self) -> Result<(), String> {
+    match self.peek(){
+        Some(Token::Soundz(_)) => self.advance(),
+        _ => return Err ("Internal Error: parse audio called without Soundz token.".to_string()),
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
+fn parse_newline(&mut self) -> Result<(), String> {
+    self.expect(&Token::Newline)?;
+    Ok(())
+}
+
+fn parse_video(&mut self) -> Result<(), String> {
+        match self.peek() {
+            Some(Token::Vidz(_)) => self.advance(),
+            _ => return Err("Internal Error: parse_video called without Vidz token.".to_string()),
+        }
+        self.expect(&Token::Mkay)?;
+        Ok(())
+    }
+    fn parse_inner_text(&mut self) -> Result<(), String> {
+        match self.peek() {
+            Some(Token::Text(_)) => self.advance(),
+            _ => return Err("Internal Error: parse_inner_text called without Text token.".to_string())
+        }
+        Ok(())
+    }
+
+fn parse_bold(&mut self) -> Result<(), String> {
+    self.expect(&Token::Bold)?;
+    match self.peek() {
+        Some(Token::Text(_)) => self.parse_inner_text()?,
+        _ => {}
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
+fn parse_italics(&mut self) -> Result<(), String> {
+    self.expect(&Token::Italics)?;
+    match self.peek() {
+        Some(Token::Text(_)) => self.parse_inner_text()?,
+        _ => {}
+    }
+    self.expect(&Token::Mkay)?;
+    Ok(())
+}
+fn parse_list(&mut self) -> Result<(), String> {
+    self.expect(&Token::List)?;
+    self.parse_list_items()?;
+    self.expect(&Token::Oic)?;
+    Ok(())
+}
+
+fn parse_list_items(&mut self) -> Result<(), String> {
+    let mut item_count = 0;
+    self.parse_comments()?;
+    while self.peek() == Some(&Token::Gimmeh) {
+        self.advance();
+        self.expect(&Token::Item)?;
+
+        while self.peek() != Some(&Token::Mkay) && self.peek().is_some() {
+            match self.peek() {
+                Some(Token::Gimmeh) => {
+                    self.advance();
+                    match self.peek() {
+                        Some(Token::Bold) => self.parse_bold()?,
+                        Some(Token::Italics) => self.parse_italics()?,
+                        _ => return Err (format!("Syntax Error: Only BOLD/ITALICS allowed in list item: {:?}", self.peek()))
+                    }
+                },
+                Some(Token::Text(_)) => self.parse_inner_text(),
+                _ => break,
+            }
+        }
+        self.expect(&Token::Mkay)?;
+        item_count += 1;
+        self.parse_comments()?;
+    }
+    if item_count == 0 {
+        return Err("Syntax Error: #MAEK LIST must contain at least one #GIMMEH ITEM.".to_string());
+    }
+    Ok(())
+}
+fn parse_inner_list(&mut self) -> Result<(), String> {
+    self.parse_list_items()
+}
+
+}
+impl SyntaxAnalyzer for Parser {
+    fn parse_lolcode(&mut self) -> Result<(), String> {
+        self.parse()
+    }
+
+    fn parse_head(&mut self) -> Result<(), String> { self.parse_head() }
+    fn parse_title(&mut self) -> Result<(), String> { self.parse_title() }
+    fn parse_comment(&mut self) -> Result<(), String> { self.parse_comments() }
+    fn parse_body(&mut self) -> Result<(), String> { self.parse_body() }
+    fn parse_paragraph(&mut self) -> Result<(), String> { self.parse_paragraph() }
+    fn parse_inner_paragraph(&mut self) -> Result<(), String> { self.parse_inner_paragraph() }
+    fn parse_inner_text(&mut self) -> Result<(), String> { self.parse_inner_text() }
+    fn parse_variable_define(&mut self) -> Result<(), String> { self.parse_variable_define() }
+    fn parse_variable_use(&mut self) -> Result<(), String> { self.parse_variable_use() }
+    fn parse_bold(&mut self) -> Result<(), String> { self.parse_bold() }
+    fn parse_italics(&mut self) -> Result<(), String> { self.parse_italics() }
+    fn parse_list(&mut self) -> Result<(), String> { self.parse_list() }
+    fn parse_list_items(&mut self) -> Result<(), String> { self.parse_list_items() }
+    fn parse_inner_list(&mut self) -> Result<(), String> { self.parse_inner_list() }
+    fn parse_audio(&mut self) -> Result<(), String> { self.parse_audio() }
+    fn parse_video(&mut self) -> Result<(), String> { self.parse_video() }
+    fn parse_newline(&mut self) -> Result<(), String> { self.parse_newline() }
 }
